480cf56062b4c67a850212a6d9a48e1ac8375bbc
Phillipa <phillipai@hotmail.com>
2023-05-03 01:41:45 +0100

    got my selectpicker working! that's the main thing lol

--- Website (Node and HBS)/public/js/signup.js
+++ Website (Node and HBS)/public/js/signup.js
@@ -1,68 +1,66 @@
-import Treeselect from 'treeselectjs'
+$(document).ready(function() {
+    showHideDateSelect()
+})
 
-const options = [
-  {
-    name: 'England',
-    value: 1,
-    children: [
-      {
-        name: 'London',
-        value: 2,
-        children: [
-          {
-            name: 'Chelsea',
-            value: 3,
-            children: []
-          },
-          {
-            name: 'West End',
-            value: 4,
-            children: []
-          }
+$("input[name=venues]").on("input", () => {
+    
+    showHideDateSelect()
+    
+
+})
+
+function showHideDateSelect() {
+    $("input[name=venues]").each((i, input) => {
+        const venueID = $(input).attr("data-venue-id")
+
+        let elems = [ // could use to then add same css to both
+            $(`label[data-venue-id=${venueID}]`),
+            $(`select[data-venue-id=${venueID}]`).parent(".bootstrap-select")
         ]
-      },
-      {
-        name: 'Brighton',
-        value: 5,
-        children: []
-      }
-    ]
-  },
-  {
-    name: 'France',
-    value: 6,
-    children: [
-      {
-        name: 'Paris',
-        value: 7,
-        children: []
-      },
-      {
-        name: 'Lyon',
-        value: 8,
-        children: []
-      }
-    ]
-  }
-]
 
-// Use slot if you need
-const slot = document.createElement('div')
-slot.innerHTML='<a class="treeselect-demo__slot" href="">Click!</a>'
+        let show = $(input).is(":checked")
+        let parentSelect = $(`select[data-venue-id=${venueID}]`).parent(".bootstrap-select")
 
-const domElement = document.querySelector('.treeselect-demo')
-const treeselect = new Treeselect({
-  parentHtmlContainer: domElement,
-  value: [4, 7, 8],
-  options: options,
-  listSlotHtmlComponent: slot
-})
+        // elems.forEach((elem) => {
 
-treeselect.srcElement.addEventListener('input', (e) => {
-  console.log('Selected value:', e.detail)
-})
+        //     $(elem).css({opacity: show ? 1 : 0})
 
-slot.addEventListener('click', (e) => {
-  e.preventDefault()
-  alert('Slot click!')
-})
+        //     if (show) {
+        //         $(elem).css({visibility: "visible"})
+
+        //     } else {
+        //         $(elem).one("transitionend", function() {
+        //             $(elem).css({visibility: "hidden"})
+        //             $(`select[data-venue-id=${venueID}]`).selectpicker("deselectAll")
+        //         })
+
+
+        //     }
+        // })
+
+        parentSelect.css("opacity", show ? 1 : 0)
+
+        if (show) {
+            parentSelect.css("visibility", "visible")
+            $(`select[data-venue-id=${venueID}]`).attr("required", true)
+            
+        } else {
+            $(`select[data-venue-id=${venueID}]`).removeAttr("required")
+
+            // using on/one transitionend means element can fade out, and picker won't be seen to deselect while still visibile
+            parentSelect.one("transitionend", function() {
+                parentSelect.css("visibility", "hidden")
+                $(`select[data-venue-id=${venueID}]`).selectpicker("deselectAll")
+            })
+
+        }
+
+        console.log($(`select[data-venue-id=${venueID}]`).find("option").prop("selected"))
+        // console.log($(`select[data-venue-id=${venueID}]`).attr)
+        // let setVis = $(input).is(":checked") ? "visible" : "hidden"
+        // let setOpac = $(input).is(":checked") ? 1 : 0
+
+        // $(`label[data-venue-id=${venueID}]`).css({ visibility: setVis, opacity: setOpac })
+        // $(`select[data-venue-id=${venueID}]`).parent(".bootstrap-select").css({ visibility: setVis, opacity: setOpac })
+    })
+}
